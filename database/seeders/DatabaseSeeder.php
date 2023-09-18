dc08068d185d8b3f2e238a4a2b8d20471f1a593c
Tareq Sifat <mdtareq.me@gmail.com>
2023-09-18 18:04:50 +0600

    project initial setup done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +23,32 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        for($i = 0; $i < 10; $i++){
+            User::create([
+                'name' => "Test User Name $i",
+                'email' => "test_$i@example.com",
+                'password' => Hash::make('password123')
+            ]);
+            for($j = 0; $j < 10; $j++){
+                Post::create([
+                    "title" => "Test Post Title i=>$i, j => $j",
+                    'body' => "Test Post Body i=>$i, j => $j",
+                    'creator' => $i,
+                    'slug' => "test-slug j=>$j",
+                    'status' => 1,
+                ]);
+                for($k=0; $k < 10; $k++){
+                    Comment::create([
+                        'post_id' => $j,
+                        'body' => "Test Comment Body i=>$i, j => $j, k => $k",
+                        'creator' => $i,
+                        'slug' => "test-slug k=>$k",
+                        'status' => 1
+                    ]);
+                }
+            }
+            
+        }
     }
 }
